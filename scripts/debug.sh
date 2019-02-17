b50473b79ea4c24c57ac0aae19a29a1f08130810
KevinPetrikov <47643163+KevinPetrikov@users.noreply.github.com>
2019-02-17 11:38:27 -0500

    2.3.1

--- scripts/debug.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-export FLUXIONDebug=1
-export FLUXIONWIKillProcesses=1
-export FLUXIONWIReloadDriver=1
-
--- scripts/preferences.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# export FLUXIONAuto=1
-# export FLUXIONDebug=1
-# export FLUXIONWIKillProcesses=1
-# export FLUXIONWIReloadDriver=1
