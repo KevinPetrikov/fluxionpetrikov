497a510b8bdc0212182eb5da39f1a6b41cef70f1
KevinPetrikov <47643163+KevinPetrikov@users.noreply.github.com>
2019-03-04 09:46:42 -0500

    Movistar Optimizacion

--- attacks/Captive Portal/sites/MovistarV2_es.portal/js2/placelabel.js
+++
@@ -1,3 +0,0 @@
-$('.form-control').on('focus blur', function (e) {
-        $(this).parents('.form-group').toggleClass('focused', (e.type === 'focus' || this.value.length > 0));
-	}).trigger('blur');
